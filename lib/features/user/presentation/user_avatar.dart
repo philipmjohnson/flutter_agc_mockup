b4a1edbc85a52aae9bc0ed4add615cc5baec2c4a
Philip Johnson <johnson@hawaii.edu>
2023-01-23 06:16:07 -1000

    Update user_avatar.dart

--- lib/features/user/presentation/user_avatar.dart
+++ lib/features/user/presentation/user_avatar.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../async_value_widget.dart';
+import '../../chapter/domain/chapter.dart';
+import '../../garden/domain/garden.dart';
+import '../../news/domain/news.dart';
 import '../application/user_providers.dart';
 import '../domain/user.dart';
+import '../domain/user_collection.dart';
 
 /// Provides a CircleAvatar with either an image if available or initials, plus a label.
 class UserAvatar extends ConsumerWidget {
@@ -13,15 +17,26 @@ class UserAvatar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    AsyncValue<User> user = ref.watch(currentUserProvider);
-    return AsyncValueWidget<User>(
-        value: user,
-        data: (user) => (user.imagePath != null)
-            ? CircleAvatar(
-                backgroundImage: AssetImage(user.imagePath!),
-              )
-            : CircleAvatar(
-                child: Text(user.initials),
-              ));
+    final String currentUserID = ref.watch(currentUserIDProvider);
+    final AsyncValue<List<User>> asyncUsers = ref.watch(usersProvider);
+    return AsyncValuesAGCWidget(
+        currentUserID: currentUserID, asyncUsers: asyncUsers, data: _build);
+  }
+
+  Widget _build(
+      {String? currentUserID,
+      List<Chapter>? chapters,
+      List<Garden>? gardens,
+      List<News>? news,
+      List<User>? users}) {
+    UserCollection userCollection = UserCollection(users);
+    User user = userCollection.getUser(userID);
+    return (user.imagePath != null)
+        ? CircleAvatar(
+            backgroundImage: AssetImage(user.imagePath!),
+          )
+        : CircleAvatar(
+            child: Text(user.initials),
+          );
   }
 }
